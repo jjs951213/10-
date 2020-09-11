7e22608e7d8e2445c101957e83c1dbd864d9d0c0
jjs951213 <jjs951213@naver.com>
2020-09-12 04:29:55 +0900

    11.8.3 TodoListItem 수정

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
