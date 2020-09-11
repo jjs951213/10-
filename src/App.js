4d0ad90dc04bf3daef81c0efe7a580ba3677a532
jjs951213 <jjs951213@naver.com>
2020-09-12 01:46:25 +0900

    10.2.3 TodoListItem 과 TodoList 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import TodoTemplate from './TodoTemplate';
-
+import TodoInsert from './TodoInsert';
+import TodoList from './TodoList';
  
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
+  );
 };
  
 export default App;
