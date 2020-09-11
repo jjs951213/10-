f46f4674481203109f38a819460afdd1cc54c9fc
jjs951213 <jjs951213@naver.com>
2020-09-12 01:36:01 +0900

    10.2.1 TodoTemplate 만들기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import TodoTemplate from './TodoTemplate';
+
  
 const App = () => {
-  return <div>Todo App을 만들자!</div>;
+  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>
 };
  
 export default App;
